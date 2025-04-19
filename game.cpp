420dd93d1205c32391600080d359660b57f255ee
jonh-edison <jportuguez@unal.edu.co>
2025-04-19 15:16:49 -0500

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,23 +1,61 @@
-/* Juego de adivinar un numero
-tener un numero 
-preguntar al usuario el numero
-informar si lo adivino o no
+/*
+Juego de adivinar un numero
+
+Tener un numero
+Preguntar al usuario el numero
+Informar si lo adivino o no
+
 */
+
 #include <iostream>
+#include <random>
+// declaration
+void play(int success, int min_val, int max_val);
+
+
 int main(void) {
-  const int NUM =10;
-  int guessed_number;
-  std::cout <<"adivina un numero entre 1 y 100:\n";
-  std::cin >> guessed_number;
-  
-  if(guessed_number== NUM)
-   {std::cout<<"GANASTE!!!\n";}
-  else if (guessed_number > NUM){ std::cout << "Te pasaste\n";
-  }
-  else{//es menor 
-    std::cout << "Te falta. \n";
-  
-  }
- 
-  return 0;
+    play(10, 1, 100);
+    play(20, 2, 22);
+
+    return 0;
 }
+
+// Implementation
+void play(int success, int min_val, int max_val)
+{
+    // reto: hacer que el numero sea aleatorio
+    std::random_device rd;
+    std::mt19937 gen(10);
+    std:: uniform_int_distribution<int> distro(min_val, max_val);
+
+    const int NUM = success;
+    
+    int guessed_number = NUM/2;
+
+    const int MIN = min_val;
+    const int MAX = max_val;
+
+    std::cout << "\nINICIA EL JUEGO\n";
+
+    while(guessed_number != NUM) {
+        std::cout << "Adivina un numero entre  " << MIN << "  y " << MAX << ":\n";
+        std::cin >> guessed_number;
+        std::cout << "Ecribiste: " << guessed_number << "\n";
+
+        // validacion del rango 
+        if (guessed_number < MIN or MAX < guessed_number) {
+            std::cout << "El numero no esta en el rango.\n";
+            continue; 
+        }
+
+        if (guessed_number == NUM) {
+            std::cout << "GANASTE!!!!\n";
+            //break;
+        } else if (guessed_number > NUM){
+            std::cout << "Te pasaste.\n";
+        } else { // es menor
+            std::cout << "Te falta.\n";
+        }
+    }
+
+}   
